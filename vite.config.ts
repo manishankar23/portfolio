bdc43b883deb1b63e49655786ef97e1742c7e184
manishankar23 <manishankar200523@gmail.com>
2025-08-22 11:30:09 +0530

    Update base URL in Vite config for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Portfolio/', // Replace 'Portfolio' with your repository name
+  base: '/portfolio/',
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
